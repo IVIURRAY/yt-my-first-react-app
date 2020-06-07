9afe5ca21415c068f7c777c30826b13ad31dcb07
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-06-07 11:06:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Software Engineer Haydn</code>
         </p>
         <a
           className="App-link"
