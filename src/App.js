39efc5b6066e5e29826cd1f5e8219df83cbbefcf
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-07-04 15:19:58 +0100

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Header, Footer } from './components/common'
 
 import './App.css';
@@ -7,14 +8,35 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <br />
-      <br />
-      <br />
-      Hello, World!
-      <br />
-      <br />
-      <br />
-      <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Router>
+          <Link to="/about">Button About</Link>
+          <br />
+          <Link to="/contact">Button Contact</Link>
+          <br />
+          <Link to="/">Button Home</Link>
+          <br />
+          <br />
+          <br />
+          <Switch>
+            <Route path="/contact">
+              Contact Me
+            </Route>
+            <Route path="/about">
+              <Footer /> 
+            </Route>
+            <Route path="/">
+              Home
+            </Route>
+          </Switch>
+        </Router>
+        <br />
+        <br />
+        <br />
+        <br />
       <Footer />
     </div>
   );
